ca175cfb0df22fd43ed96184df27aba86069fb2a
angelobrajuka <brajuka.angelo@gmail.com>
2020-06-28 21:41:25 -0400

    works, only 2 words in dictionary tho

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <sstream>
+#include <fstream>
 
 std::string getInput() {
     std::string input;
@@ -7,20 +10,32 @@ std::string getInput() {
     return input;
 }
 
-std::string translate(std::string input) {
-    char *output = (char *)input.c_str();
+std::string translate(std::string input, std::map<std::string, std::string> dictionary) {
+    return dictionary[input];
+}
 
-    for(int i = 0; i < input.length(); i-=-1) {
-        switch(output[i]) {
-            case 'c':
-                output[i] = 'n';
-                break;
+std::map<std::string, std::string> loadDictionaryFromFile(const char *file) {
+    std::map<std::string, std::string> dictionary;
+    
+    std::ifstream inFile = std::ifstream(file);
+    std::string nextLine;
+    std::istringstream iss;
+
+    if(std::getline(inFile, nextLine)) {
+        int num;
+        std::istringstream(nextLine) >> num;
+        for(int i = 0; i < num; i ++) {
+            std::getline(inFile, nextLine);
+            std::string english, stonks;
+            std::istringstream(nextLine) >> english >> stonks;
+            dictionary[english] = stonks;
         }
     }
 
-    return output;
+    return dictionary;
 }
 
 int main() {
-    std::cout << translate(getInput()) << std::endl;
+    std::map<std::string, std::string> dictionary = loadDictionaryFromFile("dictionary.txt");
+    std::cout << translate(getInput(), dictionary) << std::endl;
 }
